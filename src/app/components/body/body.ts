eaf496c1b5e7c8fda9adc8d6ce9a13fcd53532e8
Jose Arevalo <jose.arevalo@sasf.net>
2026-05-16 10:54:45 -0500

    Fix: onInit agregado para obtener cursos al renderizar la pantalla

--- src/app/components/body/body.ts
+++ src/app/components/body/body.ts
@@ -2,11 +2,13 @@ import { Component, ViewChild } from '@angular/core';
 import { CardSubject } from '../card-subject/card-subject';
 import { FormCreate } from '../form-create/form-create';
 import { FormEdit } from '../form-edit/form-edit';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import Course from '../../models/Course';
+import { Courses } from '../../services/courses';
 
 @Component({
   selector: 'app-body',
-  imports: [CardSubject, FormCreate, FormEdit, NgIf],
+  imports: [CardSubject, FormCreate, FormEdit, NgIf, NgFor],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
@@ -15,6 +17,23 @@ export class Body {
   @ViewChild(CardSubject) cardSubject!: CardSubject;
   
   modalActivo: 'c' | 'e' | null = null;
+  courses : Course[] = [];
+
+  constructor(private courService : Courses){
+
+  }
+
+  ngOnInit() : void{
+    this.courService.findAllCoures().subscribe({
+      next: (course) =>{
+        this.courses = course;
+        console.log(this.courses);
+      },
+      error: (err) =>{
+        console.log("ERROR OBTENIENDO APIS", err);
+      }
+    })
+  } 
 
   ngAfterViewInit(): void {
     this.cardSubject.editClick.subscribe(() => {
