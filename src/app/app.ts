a3f4aa233d4e48dd66a20ad33ff9b4d9e68a6b5a
Jose Arevalo <jose.arevalo@sasf.net>
2026-05-16 10:24:24 -0500

    Cambios Base

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Body } from './components/body/body';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Body, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
