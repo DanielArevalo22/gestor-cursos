15f918c2b58009fa55e0845c0fd7bb29ca76b5e9
Jose Arevalo <jose.arevalo@sasf.net>
2026-05-17 15:26:14 -0500

    Fix: Configuracion Provider

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { Body } from './components/body/body';
 import { Navbar } from './components/navbar/navbar';
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Body, Navbar, Footer],
+  imports: [Body, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('gestor-cursos');
-}
+export class App {}
