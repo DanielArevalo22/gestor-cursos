7cdaa25cb6fffb01bda603dc8d39065a9f929e3e
Jose Arevalo <jose.arevalo@sasf.net>
2026-05-17 15:25:46 -0500

    Fix: Se agrega output para el boton de eliminar y agregado el CSS del compo

--- src/app/components/card-subject/card-subject.ts
+++ src/app/components/card-subject/card-subject.ts
@@ -1,18 +1,24 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgIf } from '@angular/common';
 import Course from '../../models/Course';
 
 @Component({
   selector: 'app-card-subject',
-  imports: [],
+  imports: [NgIf],
   standalone: true,
   templateUrl: './card-subject.html',
   styleUrl: './card-subject.css',
 })
 export class CardSubject {
   @Input({ required: true }) course!: Course;
-  @Output() editClick = new EventEmitter<void>();
+  @Output() editClick = new EventEmitter<Course>();
+  @Output() deleteClick = new EventEmitter<Course>();
 
   emitEdit(): void {
-    this.editClick.emit();
+    this.editClick.emit(this.course);
+  }
+
+  emitDelete(): void {
+    this.deleteClick.emit(this.course);
   }
 }
